80c47cc525e2cdb8ca79d8d7482793094f70aa70
kentblx <129134989+kentblx@users.noreply.github.com>
2023-03-29 22:01:35 +0800

    Update sh02.sh

--- sh02.sh
+++ sh02.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 echo 正在运行
-#docker pull kentblx/test:v1 &&
-#docker container run --rm -p 8001:3000 -it kentblx/test:v1
-docker image build -t koa-demo .
-docker container run --rm -p 8001:3000 -it koa-demo
+
+#docker image build -t koa-demo .
+#docker container run --rm -p 8001:3000 -it koa-demo
+docker build . --file Dockerfile --tag my-image-name:v1
+docker container run --rm -p 8001:3000 -it my-image-name:v1
 
